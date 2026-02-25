0afe8d31404578d832ef25e6222606154ef3ecb2
neducod <nedunony16@gmail.com>
2026-02-25 04:12:40 +0100

    css gradient failed

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='flex justify-center flex-col p-6 items-center'>
-      {/* <h1>Hello, React!</h1> */}
-      <h1 className='text-5xl text-center'>Understand Anything</h1>
+    <div className='flex justify-center flex-col p-4 items-center'>
+      <h1 className='text-5xl text-center'>Understand <span>Anything</span> </h1>
       <div className='flex justify-center items-center text-center w-full p-4'>
         <p className='text-gray-500'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
       </div>
-      {/* <p className=''>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p> */}
     </div>
     
   );
