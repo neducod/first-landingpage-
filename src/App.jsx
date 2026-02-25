ecd3c5df7f4bbce6c914ee8cd69ad892e851cab4
neducod <nedunony16@gmail.com>
2026-02-25 09:38:45 +0100

    Button div

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ const App = () => {
       <div className='flex justify-center items-center text-center w-full p-4'>
         <p className='text-gray-500 font-light'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
       </div>
+      <div className='flex flex-col gap-4 justify-center items-center'>
+        <button className='p-4 rounded-lg bg-black text-white'>Try NotebookLM</button>
+        <a href="#" className='text-blue-700 underline'>Get the app</a>
+      </div>
     </div>
     
   );
