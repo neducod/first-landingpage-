5cc65425523c184826baba7e34896d3b653f2601
neducod <nedunony16@gmail.com>
2026-02-25 04:18:19 +0100

    My head hurts, last commit till noon

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
     <div className='flex justify-center flex-col p-4 items-center'>
       <h1 className='text-5xl text-center'>Understand <span>Anything</span> </h1>
       <div className='flex justify-center items-center text-center w-full p-4'>
-        <p className='text-gray-500'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
+        <p className='text-gray-500 font-light'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
       </div>
     </div>
     
