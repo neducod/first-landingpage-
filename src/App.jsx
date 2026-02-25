0dcdb5d5a5769fd15e7a765bc0ad53b5b825956b
neducod <nedunony16@gmail.com>
2026-02-25 03:42:47 +0100

    hero section started

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 const App = () => {
   return (
-    // <div className='flex justify-center items-center flex-col gap-4'></div>
-    <div>
+    <div className='flex justify-center flex-col p-6 items-center'>
       <h1>Hello, React!</h1>
+      <h1 className='text-6xl text-center'>Understand Anything</h1>
+      <p className=''>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
     </div>
     
   );
