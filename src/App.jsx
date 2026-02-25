d73a090eb0313f80d84990ee93422ebd53905018
neducod <nedunony16@gmail.com>
2026-02-25 09:53:46 +0100

    about feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,74 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='flex justify-center flex-col p-4 items-center'>
-      <h1 className='text-5xl text-center'>Understand <span>Anything</span> </h1>
-      <div className='flex justify-center items-center text-center w-full p-4'>
-        <p className='text-gray-500 font-light'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
+
+
+    <div>
+      <div className='flex justify-center flex-col p-4 items-center'>
+        <h1 className='text-5xl text-center'>Understand <span>Anything</span> </h1>
+        <div className='flex justify-center items-center text-center w-full p-4'>
+          <p className='text-gray-500 font-light'>Your research and thinking partner, grounded in the information you trust, built with the latest Gemini models.</p>
+        </div>
+        <div className='flex flex-col gap-4 justify-center items-center'>
+          <button className='p-4 pr-8 pl-8 rounded-lg bg-black text-white'>Try NotebookLM</button>
+          <a href="#" className='text-blue-700 underline'>Get the app</a>
+        </div>
       </div>
-      <div className='flex flex-col gap-4 justify-center items-center'>
-        <button className='p-4 pr-8 pl-8 rounded-lg bg-black text-white'>Try NotebookLM</button>
-        <a href="#" className='text-blue-700 underline'>Get the app</a>
+
+      <div>
+        <h2>Your AI-Powered Research Partner</h2>
+        <div className='flex flex-col'>
+          <div>
+            <div>
+              <img src="#" alt="icon" />
+              <h3>Upload your sources</h3>
+              <p>Upload PDFs, websites, YouTube videos, audio files, Google Docs, Google Slides and more, and NotebookLM will summarize them and make interesting connections between topics, all powered by the latest version of Gemini’s multimodal understanding capabilities.</p>
+            </div>
+            <img src="#" alt="" />
+          </div>
+
+          <div>
+            <div>
+              <img src="#" alt="icon" />
+              <h3>Instant insights</h3>
+              <p>With all of your sources in place, NotebookLM gets to work and becomes a personalized AI expert in the information that matters most to you.</p>
+              {/* <p>Upload PDFs, websites, YouTube videos, audio files, Google Docs, Google Slides and more, and NotebookLM will summarize them and make interesting connections between topics, all powered by the latest version of Gemini’s multimodal understanding capabilities.</p> */}
+            </div>
+            <img src="#" alt="" />
+          </div>
+
+          <div>
+            <div>
+              <img src="#" alt="icon" />
+              {/* <h3>Upload your sources</h3> */}
+              <h3>See the source, not just the answer</h3>
+              <p>Gain confidence in every response because NotebookLM provides clear citations for its work, showing you the exact quotes from your sources.</p>
+              {/* <p>Upload PDFs, websites, YouTube videos, audio files, Google Docs, Google Slides and more, and NotebookLM will summarize them and make interesting connections between topics, all powered by the latest version of Gemini’s multimodal understanding capabilities.</p> */}
+            </div>
+            <img src="#" alt="" />
+          </div>
+
+          <div>
+            <div>
+              <img src="#" alt="icon" />
+              {/* <h3>Upload your sources</h3> */}
+              <h3>Listen and learn on the go</h3>
+              <p>Our new Audio Overview feature can turn your sources into engaging “Deep Dive” discussions with one click</p>
+              {/* <p>Upload PDFs, websites, YouTube videos, audio files, Google Docs, Google Slides and more, and NotebookLM will summarize them and make interesting connections between topics, all powered by the latest version of Gemini’s multimodal understanding capabilities.</p> */}
+            </div>
+            <img src="#" alt="" />
+          </div>
+
+
+        </div>
       </div>
+
+
+
+      
+
+
+
     </div>
     
   );
