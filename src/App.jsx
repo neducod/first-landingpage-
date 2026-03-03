7ad103b76f033154259c6b0703df50e667170478
neducod <nedunony16@gmail.com>
2026-03-03 14:50:41 +0100

    deletedi image placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -105,8 +105,6 @@ return(
         <div className='flex flex-col gap-8 p-4'>
           <div className='flex flex-col gap-2' >
             <div className='flex flex-col gap-2'>
-              {/* <FaReact /> */}
-              {/* <FiAlertOctagon /> */}
               <FiUser />
               <h3 className='text-lg'>Upload your sources</h3>
               <p className='text-sm text-gray-700'>Upload PDFs, websites, YouTube videos, audio files, Google Docs, Google Slides and more, and NotebookLM will summarize them and make interesting connections between topics, all powered by the latest version of Gemini’s multimodal understanding capabilities.</p>
@@ -120,7 +118,6 @@ return(
 
           <div className='flex flex-col gap-2'>
             <div className='flex flex-col gap-2'>
-              <img src="#" alt="icon" />
               <FaBoltLightning />
               <h3 className='text-lg'>Instant insights</h3>
               <p className='text-sm text-gray-700'>With all of your sources in place, NotebookLM gets to work and becomes a personalized AI expert in the information that matters most to you.</p>
@@ -130,7 +127,6 @@ return(
 
           <div className='flex flex-col gap-2'>
             <div className='flex flex-col gap-2'>
-              <img src="#" alt="icon" />
               <FaAsterisk />
               <h3 className='text-lg'>See the source, not just the answer</h3>
               <p className='text-sm text-gray-700'>Gain confidence in every response because NotebookLM provides clear citations for its work, showing you the exact quotes from your sources.</p>
@@ -140,7 +136,6 @@ return(
 
           <div className='flex flex-col gap-2'>
             <div className='flex flex-col gap-2'>
-              <img src="#" alt="icon" />
               <LuWaves />
               <h3 className='text-lg'>Listen and learn on the go</h3>
               <p className='text-sm text-gray-700'>Our new Audio Overview feature can turn your sources into engaging “Deep Dive” discussions with one click</p>
